d2c6a54b89f8fccee5bd529190dcea022bea366d
scaulfield01 <s.caulfield88@gmail.com>
2017-08-31 10:43:43 -0700

    attempt to make audio work in desired fashion but still some issues. Test other items

--- src/components/HornContainer.js
+++ src/components/HornContainer.js
@@ -5,37 +5,55 @@ import HornButton from './HornButton';
 class HornContainer extends Component {
   constructor(props) {
     super(props);
+    // this.state = {
+    //   playStatus: 'initial', // 'A or B'
+    // }
     this.state = {
-      playStatus: 'initial', // 'A or B'
+      sounds: [],
+      currentPlay: 0,
+      loading: false
     }
     this.handleHornClick = this.handleHornClick.bind(this);
+    this.handleLoading = this.handleLoading.bind(this);
+    this.handlePlaying = this.handlePlaying.bind(this);
   }
 
+  componentWillMount() {
+    
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    // if(nextState.sounds.length > 1) {
+    // }
+    console.log(nextProps)
+    console.log(nextState)
+  } 
+
   handleHornClick(e) {
-    if (this.state.playStatus === 'initial') {
-      this.setState({ playStatus: 'A' }); 
-    } else if (this.state.playStatus === 'A') {
-      this.setState({ playStatus: 'B' });
-    } else if (this.state.playStatus === 'B') {
-      this.setState({ playStatus: 'A' });
-    }
+    this.setState({ 
+      sounds: this.state.sounds.concat({ url: 'airhornNew.mp3', playStatus:'PLAYING' }),
+      currentPlay: this.state.currentPlay += 1
+    })
+  }
+
+  handleLoading(e) {
+    // if(this.state.currentPlay === 0) {
+      debugger
+    // }
+  }
+
+  handlePlaying() {
+    // if(this.state.loading) {
+    //   this.setState({loading: this.state.loading})
+    // }
   }
 
   render() {
-    const { playStatus } = this.state;
+    const { sounds, loading } = this.state;
       return (
         <div className="HornContainer">
-          <HornButton handleHornClick={this.handleHornClick}/>
-            <Sound
-              url={'airhornNew.mp3'}
-              autoLoad={true}
-              playStatus={playStatus === 'A' ? 'PLAYING' : 'STOPPED' }
-            />
-            <Sound
-              url={'airhornNew.mp3'}
-              autoLoad={true}
-              playStatus={playStatus === 'B' ? 'PLAYING' : 'STOPPED' }
-            />
+            {this.loading ? <p>loading</p> : <HornButton handleHornClick={this.handleHornClick}/>}
+            {sounds.map((sound, index) => <Sound className="sound" url={sound.url} autoLoad={true} playStatus={sound.playStatus} onLoading={this.handleLoading()} onPlaying={this.handlePlaying} />)}
         </div>
       )
   }
